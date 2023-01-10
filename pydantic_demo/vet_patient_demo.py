9f11c8955aa3942a5fb6fd8a43480a48cb95ae96
Jakub Ostrowski <jakub.ostrowski@capgemini.com>
2023-01-10 14:41:39 +0100

    Added sample data

--- pydantic_demo/vet_patient_demo.py
+++ pydantic_demo/vet_patient_demo.py
@@ -1,21 +1,25 @@
+import json
 from datetime import datetime
+from pathlib import Path
 from uuid import UUID, uuid4
 
 from pydantic import BaseModel, EmailStr, Field
 
 
 class Pet(BaseModel):
+    """Represents a pet"""
     id: UUID = Field(default_factory=uuid4)
     name: str
     species: str
-    weight: float = None
+    weight_kg: float = None
     birthday: datetime = None
     allergies: list[str] = None
-    chipped: bool = None
+    chip: bool = None
     chip_number: int = None
 
 
 class Person(BaseModel):
+    """Represent pet's caretaker"""
     id: UUID = Field(default_factory=uuid4)
     first_name: str
     last_name: str
@@ -25,4 +29,7 @@ class Person(BaseModel):
 
 
 if __name__ == '__main__':
-    pass
+    file_name = Path('../data/vet_data.json')
+    with file_name.open() as f:
+        data = json.load(f)
+        print(json.dumps(data, indent=2))
